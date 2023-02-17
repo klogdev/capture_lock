b884ae55039b526037df38c7ec0247905f111261
EmperorAkashi <linchen4869@ucla.edu>
2023-02-16 22:27:12 -0800

    start increment by image type conversion

--- workspace/incremental_construct.cpp
+++ workspace/incremental_construct.cpp
@@ -1,7 +1,7 @@
-#include "base/database.h"
-#include "base/database_cache.h"
 #include "sfm/incremental_mapper.h"
 #include "sfm/incremental_triangulator.h"
+#include "base/image.h"
+#include "base/camera.h"
 
 #include "feature/image_sift.h"
 #include "feature/sift.h"
@@ -10,4 +10,13 @@ Image LoadImage(const std::string folder, const std::string image_name){
     std::string abs_path = folder + image_name;
     Image image(abs_path);
     return image;
+}
+
+colmap::Image SIFTtoCOLMAPImage(int image_id, std::vector<Eigen::Vector2d> features,
+                                colmap::Camera camera){
+    colmap Image curr_image;
+    curr_image.SetImageId(image_id);
+    curr_image.SetCamera(camera);
+    curr_image.SetPoints2D(features);
+    return curr_image;
 }
--- workspace/incremental_construct.h
+++ workspace/incremental_construct.h
@@ -1,5 +1,9 @@
 #include "feature/sift.h"
 #include "feature/image_sift.h"
+#include "base/image.h"
 #include <string>
 
 Image LoadImage(const std::string folder, const std::string image_name);
+
+colmap::Image SIFTtoCOLMAPImage(int image_id, std::vector<Eigen::Vector2d> features,
+                                colmap::Camera camera);
