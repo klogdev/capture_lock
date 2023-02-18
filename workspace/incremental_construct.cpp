001c3d89c9e67efa4d43490294d3d893b87bab4f
EmperorAkashi <linchen4869@ucla.edu>
2023-02-17 21:55:06 -0800

    add type converter for SIFT to COLMAP

--- workspace/incremental_construct.cpp
+++ workspace/incremental_construct.cpp
@@ -2,9 +2,12 @@
 #include "sfm/incremental_triangulator.h"
 #include "base/image.h"
 #include "base/camera.h"
+#include "base/triangulation.h"
 
 #include "feature/image_sift.h"
 #include "feature/sift.h"
+
+#include "incremental_construct.h"
 
 Image LoadImage(const std::string folder, const std::string image_name){
     std::string abs_path = folder + image_name;
@@ -14,9 +17,32 @@ Image LoadImage(const std::string folder, const std::string image_name){
 
 colmap::Image SIFTtoCOLMAPImage(int image_id, std::vector<Eigen::Vector2d> features,
                                 colmap::Camera camera){
-    colmap Image curr_image;
+    colmap::Image curr_image;
     curr_image.SetImageId(image_id);
     curr_image.SetCamera(camera);
     curr_image.SetPoints2D(features);
     return curr_image;
+}
+
+std::vector<sift::Keypoint> GetKeyPoints(Image& image){
+    const Image& image1 = image.channels == 1 ? image1 : rgb_to_grayscale(image);
+    std::vector<sift::Keypoint> key_points = sift::find_keypoints_and_descriptors(image1);
+    return key_points;
+}
+
+std::vector<Eigen::Vector2d> SIFTPtsToVector(std::vector<sift::Keypoint> key_points){
+    std::vector<Eigen::Vector2d> key_vec;
+
+    for(int i = 0; i < key_points.size(); i++){
+        int key_idx = key_points[i];
+        
+        sift::Keypoint CurrKey1 = KeyPoints1[KeyIdx1];
+        sift::Keypoint CurrKey2 = KeyPoints2[KeyIdx2];
+        Eigen::Vector2d Pt1(CurrKey1.x, CurrKey1.y);
+        Eigen::Vector2d Pt2(CurrKey2.x, CurrKey2.y);
+}
+
+std::vector<colmap::Point3D> IncrementOneImage(std::string image_path, 
+                                            const& colmap::Image){
+    Image new_image(image_path);
 }
--- workspace/incremental_construct.h
+++ workspace/incremental_construct.h
@@ -1,9 +1,21 @@
+#include <Eigen/Core>
 #include "feature/sift.h"
 #include "feature/image_sift.h"
 #include "base/image.h"
+#include "base/point3d.h"
 #include <string>
+#include "feature/sift.h"
 
 Image LoadImage(const std::string folder, const std::string image_name);
 
 colmap::Image SIFTtoCOLMAPImage(int image_id, std::vector<Eigen::Vector2d> features,
-                                colmap::Camera camera);
+                                const& colmap::Camera camera);
+
+std::vector<colmap::Point3D> IncrementOneImage(std::string image_path, 
+                                            const& colmap::Image);
+
+std::vector<sift::Keypoint> GetKeyPoints(Image& image);
+
+std::vector<Eigen::Vector2d> SIFTPtsToVector(std::vector<sift::Keypoint> key_points);
+
+    
