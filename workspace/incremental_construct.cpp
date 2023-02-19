c248e40bb4df5e268f3a6cffd1d0b5e5c5b0a173
EmperorAkashi <linchen4869@ucla.edu>
2023-02-18 22:46:01 -0800

    finished abs pose part for increment

--- workspace/incremental_construct.cpp
+++ workspace/incremental_construct.cpp
@@ -30,7 +30,7 @@ std::vector<sift::Keypoint> GetKeyPoints(Image& image){
     return key_points;
 }
 
-std::unordered_map<std::pair<int,int>, sift::keypoint> SIFTPtsToHash(std::vector<sift::Keypoint> key_points){
+std::unordered_map<std::pair<int,int>, sift::Keypoint> SIFTPtsToHash(std::vector<sift::Keypoint> key_points){
     std::unordered_map<std::pair<int,int>, sift::keypoint> key_map;
 
     for(int i = 0; i < key_points.size(); i++){
@@ -50,12 +50,48 @@ std::vector<Eigen::Vector2d> SIFTPtsToVec(std::vector<sift::Keypoint> key_points
     return key_vec;
 }
 
+std::vector<Eigen::Vector3d> GetPoint3Dfrom2D(int image_id, 
+                                            std::vector& global_image_map){
+    colmap::Image corr_image = global_image_map[image_id];
+
+}
+
 std::vector<colmap::Point3D> IncrementOneImage(std::string image_path,
                                             int next_id,
                                             const& colmap::Image last_image,
-                                            const& colmap::Camera camera){
+                                            const& colmap::Camera camera,
+                                            std::unordered_map<int,colmap::Image>& global_image_map,
+                                            std::unordered_map<int,Eigen::Vector3d>& global_keypts_map){
     Image new_image(image_path);
-    std::vector<sift::Keypoint> curr_keypts = sift::find_keypoints_and_descriptors(new_image);
+    std::vector<sift::Keypoint> curr_key_points = sift::find_keypoints_and_descriptors(new_image);
     colmap::Image new_cmp_image = SIFTtoCOLMAPImage(next_id, curr_keypts, camera);
+    int last_id = next_id - 1;
+
+    std::vector<sift::Keypoint> last_key_points = global_keypts_id[last_id];
+    std::vector<std::pair<int, int>> matches = sift::find_keypoint_matches(last_key_points, curr_key_points);
+    //covert sift keypts to eigen, should we only pick matched 2d pts for pose est??
+    std::vector<Eigen::Vector2d> curr_keypts_vec = SIFTPtstoVec(curr_key_points);
+
+    std::vector<Eigen::Vector3d> matched3d_from2d;
+    for (int i = 0; i < matches.size(); i++){
+        //assume key_points id for last image are consistent with the id
+        //registered in points2d
+        colmap::point2D_t curr_2d_id = matches[i].first; //need type conversion
+        colmap::Point2D curr_2d = last_image.Point2D(curr_2d_id);
+        colmap::point3D_t global_3d_key = curr_2d.Point3DId();
+        Eigen::Vector3d from2d_to3d = global_keypts_map[global_3d_key];
+        matched3d_from2d.push_back(from2d_to3d);
+    }
+
+    colmap::AbsolutePoseEstimationOptions absolute_options = colmap::AbsolutePoseEstimationOptions();
+    absolute_options.ransac_options.max_error = 0.05;
+    Eigen::Vector4d qvec_abs = Eigen::Vector4d(0, 0, 0, 1);
+    Eigen::Vector3d tvec_abs = Eigen::Vector3d::Zero();
+    std::vector<char> inlier_mask;
+    bool abs_pose = colmap::EstimateAbsolutePose(absolute_options, curr_keypts_vec,
+                                                matched3d_from2d, &qvec_abs, &tvec_abs,
+                                                &camera1, &inliers, &inlier_mask);
+    new_image.SetQvec(qvec_abs);
+    new_image.SetTvec(tvec_abs);
 
 }
--- workspace/incremental_construct.h
+++ workspace/incremental_construct.h
@@ -14,16 +14,21 @@ colmap::Image SIFTtoCOLMAPImage(int image_id, std::vector<Eigen::Vector2d> featu
 std::vector<colmap::Point3D> IncrementOneImage(std::string image_path,
                                             int next_id,
                                             const& colmap::Image last_image,
-                                            const& colmap::Camera camera);
+                                            const& colmap::Camera camera,
+                                            std::unordered_map<int,colmap::Image>& global_image_map,
+                                            std::unordered_map<int,Eigen::Vector3d>& global_keypts_map);
 
 //generate sift keypoints from sift image
 std::vector<sift::Keypoint> GetKeyPoints(Image& image);
 
 //get a hash map of <tuple,sift::keypoint>, use (i,j) pair to connect
 //with Point2D in colmap::Image
-std::unordered_map<std::pair<int,int>, sift::keypoint> SIFTPtsToHash(std::vector<sift::Keypoint> key_points);
+std::unordered_map<std::pair<int,int>, sift::Keypoint> SIFTPtsToHash(std::vector<sift::Keypoint> key_points);
 
 //convert i,j coord from SIFT points to eigen vector for the init of colmap::Image
 std::vector<Eigen::Vector2d> SIFTPtsToVec(std::vector<sift::Keypoint> key_points);
+
+std::vector<Eigen::Vector3d> GetPoint3Dfrom2D(int image_id, 
+                                            std::vector& global_image_map);
 
     
