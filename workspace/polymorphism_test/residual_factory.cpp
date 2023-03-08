2301e7877bd625a65968b1d523c22ae0b93dd53b
EmperorAkashi <linchen4869@ucla.edu>
2023-03-07 22:20:24 -0800

    finish derived class for res calc; need main

--- workspace/polymorphism_test/residual_factory.cpp
+++ workspace/polymorphism_test/residual_factory.cpp
@@ -1,4 +1,5 @@
 #include <ceres/ceres.h>
+#include <Eigen/Core>
 
 class ResidualCalculator{
     public:
@@ -7,10 +8,32 @@ class ResidualCalculator{
 
 class ReprojectionError : public ResidualCalculator{
     public:
-        ReprojectionError(double obs_x, double obs_y):
-            obs_x_(obs_x), obs_y_(obs_y){}
+        ReprojectionError(Eigen::Vector2d point_2d, Eigen::Vector3d point_3d):
+            obs_x_(point_2d(0)), obs_y_(point_2d(1)){}
     
-    virtual bool Evaluate(double* residuals, double* parameters, double* jacobians) override{
-        
+    virtual bool Evaluate(std::vector<double>* residuals, std::vector<double>* parameters,
+                        double* jacobians) override{
+        //parameters should be a flattened vector with size 4x3
+        Eigen::Vector3d project_3d;
+        Eigen::Vector4d point3d_homo = Eigen::Vector4d::Identity();
+        point3d_homo.topRows(3) = point_3d; 
+
+        Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> proj_mat(parameters.data(), 3, 4);
+        project_3d = proj_mat*point3d_homo;
+
+
+        residuals[0] = obs_x_ - project_3d(0)/project_3d(2);
+        residuals[1] = obs_y_ - project_3d(1)/project_3d(2);
+
+        //if jacobian != nullptr: update paras via ceres
     }
+
+    private:
+        double obs_x_;
+        double obs_y_;
+}
+
+int main(){
+    ceres::Problem problem;
+
 }
