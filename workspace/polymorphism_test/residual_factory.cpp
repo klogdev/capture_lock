002eed3841aad023e56a0426955cfe893f1fed1c
Charlie <klog.dev.team@gmail.com>
2023-03-09 13:17:02 -0800

    Use of ceres::AutoDiffCostFunction.

--- workspace/polymorphism_test/residual_factory.cpp
+++ workspace/polymorphism_test/residual_factory.cpp
@@ -3,45 +3,70 @@
 
 class ResidualCalculator{
     public:
-        virtual bool Evaluate(const std::vector<double>* residuals, const std::vector<double>* parameters) const = 0;
+     virtual bool Evaluate(std::vector<double>* residuals,
+                           const std::vector<double>* parameters) const = 0;
 };
 
 class ReprojectionError : public ResidualCalculator{
     public:
-        ReprojectionError(Eigen::Vector2d point_2d, Eigen::Vector3d point_3d):
-                    camera_point(point_2d), world_point(point_3d){}
-    
-    virtual bool Evaluate(std::vector<double>* residuals, std::vector<double>* parameters) const override{
-        //parameters should be a flattened vector with size 4x3
-        Eigen::Vector3d project_3d;
-        Eigen::Vector4d point3d_homo = Eigen::Vector4d::Identity();
-        point3d_homo.topRows(3) = world_point; 
+     ReprojectionError(const Eigen::Vector2d& point_2d,
+                       const Eigen::Vector3d& point_3d)
+         : camera_point(point_2d), world_point(point_3d) {}
 
-        Eigen::Map<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> proj_mat(parameters->data(), 3, 4);
-        project_3d = proj_mat*point3d_homo;
+     bool Evaluate(std::vector<double>* residuals,
+                   const std::vector<double>* parameters) const override {
+       // parameters should be a flattened vector with size 4x3
+       Eigen::Vector3d project_3d;
+       Eigen::Vector4d point3d_homo = Eigen::Vector4d::Identity();
+       point3d_homo.topRows(3) = world_point;
 
+       Eigen::Map<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
+                                      Eigen::RowMajor>>
+           proj_mat(parameters->data(), 3, 4);
+       project_3d = proj_mat * point3d_homo;
 
-        (*residuals)[0] = camera_point(0) - project_3d(0)/project_3d(2);
-        (*residuals)[1] = camera_point(1) - project_3d(1)/project_3d(2);
+       (*residuals)[0] = camera_point(0) - project_3d(0) / project_3d(2);
+       (*residuals)[1] = camera_point(1) - project_3d(1) / project_3d(2);
 
-        return true;
-    }
+       return true;
+     }
 
     private:
         Eigen::Vector2d camera_point;
         Eigen::Vector3d world_point;
 };
 
+class ReprojectionErrorFunctor {
+ public:
+  ReprojectionErrorFunctor(const std::array<double, 12>& some_param)
+      : some_param_(some_param) {}
+
+  template <typename T>
+  bool operator()(const T* const x, const T* const y, T* e) const {
+    // Just an example test, no practical meaning.
+    e[0] = x[0] * y[0] + x[1] * y[1] + y[2];
+    e[1] = some_param_[0] * x[0] * y[1] + x[1] * y[2];
+    return true;
+  }
+
+ private:
+  std::array<double, 12> some_param_;
+};
+
 int main(){
     ceres::Problem problem;
     Eigen::Vector2d camera_point(100.0, 120.0);
     Eigen::Vector3d triangulated_point(1.0, 2.0, 3.0);
-    ceres::CostFunction* cost_function =
-      new ceres::AutoDiffCostFunction<ReprojectionError, 2, 12, 2, 3>(
-          new ReprojectionError(camera_point, triangulated_point));
+    // ceres::CostFunction* cost_function =
+    //   new ceres::AutoDiffCostFunction<ReprojectionError, 2, 12, 2, 3>(
+    //       new ReprojectionError(camera_point, triangulated_point));
 
-    problem.AddResidualBlock(cost_function, nullptr, new double[12],
-                        camera_point.data(), triangulated_point.data());
+    std::array<double, 12> some_param = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    ceres::CostFunction* cost_function =
+        new ceres::AutoDiffCostFunction<ReprojectionErrorFunctor, 2, 2, 3>(
+            new ReprojectionErrorFunctor(some_param));
+    problem.AddResidualBlock(cost_function, nullptr, camera_point.data(),
+                             triangulated_point.data());
 
     // Set the solver options
     ceres::Solver::Options options;
