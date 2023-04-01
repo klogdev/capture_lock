9806da38eb2f6dd502d82e4e0067cbf4c7f445cf
EmperorAkashi <linchen4869@ucla.edu>
2023-03-31 22:04:04 -0700

    add helper function for homo coordinate calc

--- workspace/epipole_estimator.h
+++ workspace/epipole_estimator.h
@@ -5,11 +5,20 @@
 
 class EpipoleEstimator{
     public: 
-        struct PointData{
-            PointData(){}
-
-            PointData(const Eigen::Vector2d& point_0, const Eigen::Vector2d& point_1)
-                : point_lock(point_0), point_map(point_1) {}
-
-        }
+        
 };
+
+//helper functions
+Eigen::Vector3d Point2dToHomo(Eigen::Vector2d point_2d){
+    homo = Eigen::Vector4d::Identity();
+    homo.topRows(2) = point_2d;
+    return homo; 
+}
+
+Eigen::Vector3d PointToLine(Eigen::Vector3d pixel1, Eigen::Vector3d pixel2){
+    return pixel1.cross(pixel2);
+}
+
+Eigen::Vector3d LineToPoint(Eigen::Vector3d line1, Eigen::Vector3d line2){
+    return line1.cross(line2);
+}
