6614e8ea8da705e14d5c1f71b9d2accd9f41b82c
EmperorAkashi <linchen4869@ucla.edu>
2023-12-31 23:52:50 -0800

    reorg gyro data loader

---
+++ workspace/file_reader/gyro_raw.h
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <string>
+#include <fstream>
+#include <Eigen/Core>
+
+#include <absl/random/random.h>
+#include <absl/strings/str_format.h>
+
+/**
+ * @brief encapsulate the kitti raw imu's gyro data as a structure
+*/
+struct GyroData {
+    double timestamp;
+    double wx, wy, wz;
+};
+
+std::vector<double> LoadTimeStamp(std::string timestamp);
+
+Eigen::Vector3d LoadOneGyro(std::string one_file_path);
+
+std::vector<GyroData> LoadGyroData(std::string timestamp_path, 
+                                   std::string data_path,
+                                   int start_frame,
+                                   int end_frame);
+
+std::vector<std::string> GyroPathStream(const std::string folder_dir);
+
+//general fxn for obtaining all file paths of images in a folder
+std::vector<std::vector<std::string>> FilePathStream(const std::string folder_dir);
+
+double TimeStrToDouble(std::string timestamp);
--- workspace/file_stream.h
+++ workspace/file_stream.h
@@ -6,23 +6,7 @@
 #include <absl/random/random.h>
 #include <absl/strings/str_format.h>
 
-struct GyroData {
-    double timestamp;
-    double wx, wy, wz;
-};
-
-std::vector<double> LoadTimeStamp(std::string timestamp);
-
-Eigen::Vector3d LoadOneGyro(std::string one_file_path);
-
-std::vector<GyroData> LoadGyroData(std::string timestamp_path, 
-                                   std::string data_path,
-                                   int start_frame,
-                                   int end_frame);
-
-std::vector<std::string> GyroPathStream(const std::string folder_dir);
-
-//general fxn for obtaining all file paths of images in a folder
+/**
+ * @brief load list of colmap images as a stream
+*/
 std::vector<std::vector<std::string>> FilePathStream(const std::string folder_dir);
-
-double TimeStrToDouble(std::string timestamp);
