84de0e1babcc20c28797f580676f01d1d2af6c1c
EmperorAkashi <linchen4869@ucla.edu>
2024-11-02 21:08:25 -0700

    add conversion of tum file name

--- workspace/pnp/pnp_test.cpp
+++ workspace/pnp/pnp_test.cpp
@@ -24,6 +24,8 @@ int main(int argc, char** argv) {
     // so we set it as -1 as a dummy value
     double sigma = -1.0;
 
+    std::cout << argv[5] << std::endl;
+
     if(argc >= 5) {
         sigma = std::stod(argv[4]);
         if(generator_opt == "epnp_dz")
@@ -34,7 +36,8 @@ int main(int argc, char** argv) {
 
     if(generator_opt == "tum_rgbd") {
         if(argc >= 5)
-            TumRgbd::curr_data = argv[4];
+            std::string tum_file = argv[4];
+            TumRgbd::curr_data = tum_file;
     }
 
     bool lhm_type = false;
